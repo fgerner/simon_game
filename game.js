f1812bdcd97be08612d543c86f48cd4d51812dff
Fred <fred.gerner@icloud.com>
2020-05-03 10:44:03 +1000

    added animation to user press

--- game.js
+++ game.js
@@ -5,16 +5,28 @@ let userClickedPattern = []
 $(".btn").click(function () {
     let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
-})
+    playSound(userChosenColor)
+});
 
 function nextSequence() {
     let randomNumber = Math.round(Math.random() * 3);
     let randChosenColor = buttonColors[randomNumber];
     gamePattern.push(randChosenColor);
     $("#" + randChosenColor).fadeOut(100).fadeIn(100).fadeOut(100)
+    playSound(randChosenColor);
+}
 
-    let audio = new Audio("sounds/" + randChosenColor + ".mp3");
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).classList.add("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).classList.remove("pressed")
+    }, 100);
 }
 
