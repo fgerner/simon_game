44042472d91284fb094bacddfae6fee315ee97ba
Fred <fred.gerner@icloud.com>
2020-05-03 10:14:41 +1000

    Added event handler for userchoice of colors and moved jquery cdn

--- game.js
+++ game.js
@@ -1,13 +1,20 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = []
+
+$(".btn").click(function () {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+})
 
 function nextSequence() {
     let randomNumber = Math.round(Math.random() * 3);
     let randChosenColor = buttonColors[randomNumber];
     gamePattern.push(randChosenColor);
-    $("#"+randChosenColor).fadeOut(100).fadeIn(100).fadeOut(100)
+    $("#" + randChosenColor).fadeOut(100).fadeIn(100).fadeOut(100)
 
     let audio = new Audio("sounds/" + randChosenColor + ".mp3");
     audio.play();
-};
+
+}
 
